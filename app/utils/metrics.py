7601e35d37ef001ef4a82a77f51076014d1cb26a
Anushtha <kumarsen.bash@gmail.com>
2024-06-25 22:56:38 +0530

    salincy fix

--- app/utils/metrics.py
+++ app/utils/metrics.py
@@ -20,7 +20,8 @@ def calculate_far_p_duration(far_p_duration, weightings):
     return (far_p_duration*weightings)/60
 
 def calculate_saliency(total):
-    return total / 100
+    #return total / 100
+    return total
 
 def calculate_net_saliency(saliency_front, saliency_rear):
     return (saliency_front + saliency_rear) / 2
